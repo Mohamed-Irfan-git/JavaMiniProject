3c2cd9a6ff9c496b66561f9336ce0c9bb9d112a2
Mohamed-Irfan-git <irfanmt29@gmail.com>
2026-02-26 15:59:45 +0530

    database initialization done

--- backend/src/main/java/command/LoginCommand.java
+++ backend/src/main/java/command/LoginCommand.java
@@ -1,9 +1,26 @@
 package command;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dto.requestDto.LoginRequestDTO;
+
 public class LoginCommand implements Command {
 
+    private final ObjectMapper mapper = new ObjectMapper();
+
     @Override
-    public void execute(String[] args, ClientContext context) {
-        context.getOutput().println("Login successful");
+    public void execute(Object data, ClientContext context) {
+
+
+        LoginRequestDTO login =
+                mapper.convertValue(data, LoginRequestDTO.class);
+
+        String username = login.getUsername();
+        String password = login.getPassword();
+
+        // TODO: call service layer
+
+        context.getOutput().println(
+                "{\"status\":\"success\",\"message\":\"Login successful\"}"
+        );
     }
 }
---
+++ backend/src/main/java/dto/requestDto/RequestDTO.java
@@ -0,0 +1,23 @@
+package dto.requestDto;
+
+public class RequestDTO {
+    private String command;
+    private Object data;
+
+    public String getCommand() {
+        return command;
+    }
+
+    public void setCommand(String command) {
+        this.command = command;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+}
